b55cfc3136d3ff0dc8b948adbb4829954859529c
AdamVasina <adamvasina@gmail.com>
2025-06-17 19:52:05 +0200

    BDD FIRST COMIT

---
+++ src/main/java/PokusBDD.java
@@ -0,0 +1,31 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PokusBDD {
+    @Given("nastavenie pociatocnych podmienok")
+    public void nastaveniePociatocnychPodmienok() {
+
+    }
+
+    @And("dalsi nastavovaci krok")
+    public void dalsiNastavovaciKrok() {
+    }
+
+    @When("toto je nejaka akce")
+    public void totoJeNejakaAkce() {
+    }
+
+    @And("toto je dalsia akce")
+    public void totoJeDalsiaAkce() {
+    }
+
+    @Then("overenie vysledku")
+    public void overenieVysledku() {
+    }
+
+    @And("dalsie overenie vysledku")
+    public void dalsieOverenieVysledku() {
+    }
+}
---
+++ src/main/resources/pokus.feature
@@ -0,0 +1,16 @@
+Feature: pokus
+
+
+  Scenario: Pokusny test 1
+    Given nastavenie pociatocnych podmienok
+    And dalsi nastavovaci krok
+    When toto je nejaka akce
+    And toto je dalsia akce
+    Then overenie vysledku
+    And dalsie overenie vysledku
+
+
+    Scenario: Pokusny test 2
+      Given nastavenie pociatocnych podmienok
+      When toto je dalsia akce
+      Then dalsie overenie vysledku
